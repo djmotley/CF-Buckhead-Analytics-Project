3bacc11a210e4dc2a6dc72f92e9a0b7c974a7ab0
Derek Motley <djmotley001@gmail.com>
2026-01-27 12:38:41 -0500

    Add project root to Python path

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+import sys
+import os
+
+# Add the project root to Python path
+sys.path.append(os.path.dirname(__file__))
+
 from cf_buckhead_analytics.dashboard import run_dashboard_app
+import streamlit as st
+
+st.set_page_config(page_title="CF Buckhead Analytics Dashboard")
 
 if __name__ == "__main__":
     run_dashboard_app()
